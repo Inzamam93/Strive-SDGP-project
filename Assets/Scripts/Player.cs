740ddf84f71e90b2d768740d5e89baddc8b99e53
Inzamam Ghazzali <inzamam99@gmail.com>
2017-03-25 12:59:47 +0530

    Strive.
    Strive is an activity based 2D side-scroller game. The game
    protagonist's attributes such health, strength and stamina are derived
    based on the user's daily step-count. This concept is meant to motivate
    the user to be more active and walk more frequently before initiating
    game play.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,29 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    //walk variables
     public float walkSpeed;
     public float runSpeed;
+    //jump variables
     public float jumpSpeed;
-    public bool grounded;
-    //public Vector2 velocity;
+    public bool grounded = false;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
+    
     public Rigidbody2D rb2D;
     bool facingRight;
     Animator anim;
+
+
 
     // Use this for initialization
     void Start () {
         rb2D = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         //facingRight = true;
+       // rb2D.freezeRotation = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    private void Update()
+    {
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            //grounded in animator parameter
+            anim.SetBool("grounded", grounded);
+            rb2D.AddForce(new Vector2(0, jumpHeight));
+
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        // check if character is grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius, groundLayer);//drawing a circle and checking if it intersects ground.
+        //if false, then player is falling
+        anim.SetBool("grounded", grounded);
+
+        anim.SetFloat("verticalVelocity", rb2D.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
+      
         // rb2D.AddForce((Vector2.right*speed)*h);
         anim.SetFloat("walkSpeed", Mathf.Abs(move));
-
+        anim.SetBool("jumpSpeed", grounded);
         rb2D.velocity=new Vector2(move* walkSpeed, rb2D.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(Vector2.up * jumpSpeed);
+        }
 
         if(move<0 && !facingRight)
         {
