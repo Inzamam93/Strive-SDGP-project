6d1f79172f74256dad4d0aeca7da3934f6c13e4c
viduraamarasinghe <viduraamarasinghe@outlook.com>
2017-04-24 18:36:37 +0530

    Add files via upload

---
+++ MoveStatic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStatic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        //rigidbody.Transform(Input.getAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+	}
+}
