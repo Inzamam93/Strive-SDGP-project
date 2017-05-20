eb8a2de4248a61e95b5da55c7bf39f34aa6732f5
Inzamam Ghazzali <inzamam99@gmail.com>
2017-05-20 14:21:14 +0530

    Added files via upload
    Added Enemy health, enemy damage and player health files

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+    public float enemyMaxHealth;
+    float currentHealth;
+	// Use this for initialization
+	void Start () {
+        currentHealth = enemyMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Method to allow enemy to receive damage
+    public void addDamage(float damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            enemyDie();
+        }
+    }
+
+    void enemyDie()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/playerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+    public float maxPlayerHealth;
+    float currentHealth;
+    Player playerControl;
+    public GameObject deathFX;
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxPlayerHealth;
+
+        playerControl = GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void addDamage(float damage)
+    {
+        if (damage <= 0) return;
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            playerDie();
+        }
+    }
+
+    public void playerDie()
+    {
+        //Instantiate( object to be instantiated, where/location to instantiate, rotation of object) 
+        Instantiate(deathFX, transform.position, transform.rotation);
+        Destroy(gameObject);
+    } 
+}
